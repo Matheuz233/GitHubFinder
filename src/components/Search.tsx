b3fee6c5d2293e94e20ca3af94f16294db151fc5
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-28 17:52:42 -0300

    feat: :hammer: Search Component and Search Function

---
+++ src/components/Search.tsx
@@ -0,0 +1,30 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+};
+
+import { useState } from "react";
+
+import { BsSearch } from "react-icons/bs";
+
+function Search({ loadUser }: SearchProps) {
+  const [userName, setUserName] = useState("");
+
+  return (
+    <div>
+      <h2>Busque por Usuário</h2>
+      <p>Conheça seus melhores repositórios</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { UserProps } from "../types/User";
+
+import { useState } from "react";
+
+import Search from "../components/Search";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [user, setUser] = useState<UserProps | null>(null); // User inicia como UserProps ou Null
 
-export default Home
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+
+    console.log(data)
+  };
+
+  return (
+    <div>
+      <Search loadUser = {loadUser} />
+    </div>
+  );
+};
+
+export default Home;

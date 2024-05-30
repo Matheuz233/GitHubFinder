0767772c71412d4b027cd5fd11356e6c75253aa2
Matheus Augusto <matheusaugus233@gmail.com>
2024-05-30 13:52:41 -0300

    feat: :hammer: Error Handling / User Component Styles

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,6 +3,7 @@ import { UserProps } from "../types/User";
 import { MdLocationPin } from "react-icons/md";
 
 import { Link } from "react-router-dom";
+import classes from "./User.module.css";
 
 const User = ({
   avatar_url,
@@ -12,26 +13,26 @@ const User = ({
   following,
 }: UserProps) => {
   return (
-    <div>
+    <div className={classes.user}>
       <img src={avatar_url} alt={login} />
       <h2>{login}</h2>
       {location && (
-        <p>
+        <p className={classes.location}>
           <MdLocationPin />
           <span>{location}</span>
         </p>
       )}
-      <div>
+      <div className={classes.stats}>
         <div>
           <p>Seguidores:</p>
-          <p>{followers}</p>
+          <p className={classes.number}>{followers}</p>
         </div>
         <div>
           <p>Seguindo:</p>
-          <p>{following}</p>
+          <p className={classes.number}>{following}</p>
         </div>
       </div>
-      <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
+      <Link to={`https://github.com/${login}`}>Visualizar Perfil Completo</Link>
     </div>
   );
 };
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,14 +4,24 @@ import { useState } from "react";
 
 import Search from "../components/Search";
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null); // User inicia como UserProps ou Null
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -30,6 +40,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && !user && <Error />}
     </div>
   );
 };
